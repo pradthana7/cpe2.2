140869ff124f94656e06ec1f3af19a756a9389dd
pradthana7 <77404881+pradthana7@users.noreply.github.com>
2022-02-27 21:36:21 +0700

    Add files via upload

---
+++ OOT/OOT_B6322113/src/idAndNameCheck/StudentIDFormatException.java
@@ -0,0 +1,7 @@
+package week11.idAndNameCheck;
+
+public class StudentIDFormatException extends Exception{
+    public StudentIDFormatException(String str){
+        super(str);
+    }
+}
---
+++ OOT/OOT_B6322113/src/idAndNameCheck/StudentNameFormatException.java
@@ -0,0 +1,7 @@
+package week11.idAndNameCheck;
+
+public class StudentNameFormatException extends Exception{
+    public StudentNameFormatException(String str){
+        super(str);
+    }
+}
